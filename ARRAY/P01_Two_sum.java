1fc884d8c82b2d4558a409953f00dc35dc2fe89b
Riyanshu Sharma <riyanshu062@gmail.com>
2025-08-11 16:42:04 +0530

    Two Sum

---
+++ ARRAY/P01_Two_sum.java
@@ -0,0 +1,23 @@
+package ARRAY;
+
+public class P01_Two_sum {
+    
+}
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+         int n = numbers.length;
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1}; 
+            } else if (sum > target) {
+                high--;
+            } else {
+                low++;
+            }
+        }
+        return new int[]{}; 
+    }
+}
