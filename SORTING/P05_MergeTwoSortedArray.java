4d80a20d345f1f3444b45a4461fe13f3ebeb4642
Anshu <riyanshu062@gmail.com>
2025-08-23 15:08:50 +0530

    done

--- SORTING/P05_MergeTwoSortedArray.java
+++ SORTING/P05_MergeTwoSortedArray.java
@@ -1,5 +1,16 @@
 package SORTING;
 
+import java.util.Arrays;
+
 public class P05_MergeTwoSortedArray {
-    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        // Copy nums2 into nums1 starting from index m
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+
+        // Sort the final nums1
+        Arrays.sort(nums1);
+    }
 }
