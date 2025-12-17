818af0ad1949247144e9992bcb54c56ed902cfcf
Anshu <riyanshu062@gmail.com>
2025-12-17 21:48:13 +0530

    Buy and Sell

---
+++ ARRAY/P06_Buy_Sell.java
@@ -0,0 +1,19 @@
+package ARRAY;
+
+public class P06_Buy_Sell {
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
