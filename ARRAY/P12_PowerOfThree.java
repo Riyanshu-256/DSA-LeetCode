6846b5b84416f4bfe759040ce288e11720db1eb1
Anshu <riyanshu062@gmail.com>
2025-12-19 16:14:38 +0530

    Power of 3

--- ARRAY/P12_PowerOfThree.java
+++ ARRAY/P12_PowerOfThree.java
@@ -1,5 +1,10 @@
 package ARRAY;
 
 public class P12_PowerOfThree {
-    
+    public boolean isPowerOfThree(int n) {
+        if(n==1) return true;  // power(3, 0) = 1
+        if(n <= 0 || n % 3 != 0) return false;
+
+        return isPowerOfThree(n/3);
+    }
 }
