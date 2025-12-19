f2674063708fd678acc18722929b2b9eb2389069
Anshu <riyanshu062@gmail.com>
2025-12-19 16:15:15 +0530

    Power of 2

--- ARRAY/P11_PowerOfTwo.java
+++ ARRAY/P11_PowerOfTwo.java
@@ -1,5 +1,10 @@
 package ARRAY;
 
 public class P11_PowerOfTwo {
-    
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;  // power(2, 0) = 1
+        if(n <= 0 || n % 2 != 0) return false;
+
+        return isPowerOfTwo(n/2);
+    }
 }
