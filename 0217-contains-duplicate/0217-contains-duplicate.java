31d4ddeb876810fbca9428feba347f7325dda50f
Riyanshu Sharma <riyanshu062@gmail.com>
2026-07-12 21:00:26 +0530

    Time: 43 ms (5.1%), Space: 110.2 MB (5.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,20 +1,39 @@
 // EFFICIENT APPROACH -> using HashMap
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-
-// BROUTE APPROACH
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // code here
 
-        Arrays.sort(nums);
-        int n = nums.length;
+        // Create a hashmap that store element and its frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<n-1; i++){
-            if(nums[i] == nums[i+1]){
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for(int key : map.keySet()){
+            if(map.get(key) >= 2){
                 return true;
             }
         }
         return false;
     }
 }
+
+// --------------------------------------------------------------------------------- //
+
+// // BROUTE APPROACH
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         // code here
+
+//         Arrays.sort(nums);
+//         int n = nums.length;
+
+//         for(int i=0; i<n-1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
