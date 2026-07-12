02bf2f60e6047c200f65a94ba24e68baef764859
Riyanshu Sharma <riyanshu062@gmail.com>
2026-07-12 20:18:24 +0530

    Time: 2 ms (99.31%), Space: 47.4 MB (10.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,40 @@
 // EFFICIENT APPROACH -> using HashMap
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//     }
-// }
-
-
-// BROUTE APPROACH 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // code here
 
+        // Create a hashset to store nums element
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                int sum = nums[i] + nums[j];
 
-                if(sum == target){
-                    return new int[]{i,j};
-                }
+        for(int i=0; i<n; i++){
+            int need = target - nums[i];
+
+            if(map.containsKey(need)){
+                return new int[]{map.get(need) , i};
             }
+            map.put(nums[i], i);
         }
         return new int[]{-1,-1};
     }
 }
+
+
+// BROUTE APPROACH 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         // code here
+
+//         int n = nums.length;
+//         for(int i=0; i<n-1; i++){
+//             for(int j=i+1; j<n; j++){
+//                 int sum = nums[i] + nums[j];
+
+//                 if(sum == target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return new int[]{-1,-1};
+//     }
+// }
