f2aa528a80506f72922234f0a2fb21cabd788a13
Riyanshu Sharma <riyanshu062@gmail.com>
2026-07-23 16:54:22 +0530

    Time: 1 ms (5.27%), Space: 43 MB (64.62%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
@@ -13,64 +13,67 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode increasingBST(TreeNode root) {
-//         // code here
 
-//         ArrayList<Integer> list = new ArrayList<>();
-
-//         preorder(root, list);
-
-//         Collections.sort(list);
-//         int n = list.size();
-
-//         TreeNode newRoot = new TreeNode(list.get(0));
-//         TreeNode curr = newRoot;
-
-//         for(int i=1; i<n; i++){
-//             curr.right = new TreeNode(list.get(i));
-//             curr = curr.right;
-//         }
-
-//         return newRoot;
-//     }
-
-//     public void preorder(TreeNode root, ArrayList<Integer> list){
-
-//         if(root == null) return;
-
-//         list.add(root.val);
-//         preorder(root.left, list);
-//         preorder(root.right, list);
-
-//     }
-// }
-
-
+// O(nlogn)
 class Solution {
-
-    TreeNode dummy = new TreeNode(-1);
-    TreeNode curr = dummy;
-
     public TreeNode increasingBST(TreeNode root) {
+        // code here
 
-        inorder(root);
+        ArrayList<Integer> list = new ArrayList<>();
 
-        return dummy.right;
-    }
+        preorder(root, list);
 
-    public void inorder(TreeNode root) {
+        Collections.sort(list);
+        int n = list.size();
 
-        if (root == null) {
-            return;
+        TreeNode newRoot = new TreeNode(list.get(0));
+        TreeNode curr = newRoot;
+
+        for(int i=1; i<n; i++){
+            curr.right = new TreeNode(list.get(i));
+            curr = curr.right;
         }
 
-        inorder(root.left);
+        return newRoot;
+    }
 
-        root.left = null;
-        curr.right = root;
-        curr = root;
+    public void preorder(TreeNode root, ArrayList<Integer> list){
 
-        inorder(root.right);
+        if(root == null) return;
+
+        list.add(root.val);
+        preorder(root.left, list);
+        preorder(root.right, list);
+
     }
 }
+
+
+// O(n)
+// class Solution {
+
+//     TreeNode dummy = new TreeNode(-1);
+//     TreeNode curr = dummy;
+
+//     public TreeNode increasingBST(TreeNode root) {
+
+//         inorder(root);
+
+//         return dummy.right;
+//     }
+
+//     public void inorder(TreeNode root) {
+
+//         if (root == null) {
+//             return;
+//         }
+
+//         inorder(root.left);
+
+//         root.left = null;
+//         curr.right = root;
+//         curr = root;
+
+//         inorder(root.right);
+//     }
+// }
