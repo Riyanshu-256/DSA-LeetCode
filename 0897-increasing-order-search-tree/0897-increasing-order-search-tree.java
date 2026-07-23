5288c9b1cb5f07677ed24af94f7d168f1d9aca2a
Riyanshu Sharma <riyanshu062@gmail.com>
2026-07-23 16:53:13 +0530

    Time: 0 ms (100%), Space: 43 MB (43.53%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.java
@@ -13,35 +13,64 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode increasingBST(TreeNode root) {
+//         // code here
+
+//         ArrayList<Integer> list = new ArrayList<>();
+
+//         preorder(root, list);
+
+//         Collections.sort(list);
+//         int n = list.size();
+
+//         TreeNode newRoot = new TreeNode(list.get(0));
+//         TreeNode curr = newRoot;
+
+//         for(int i=1; i<n; i++){
+//             curr.right = new TreeNode(list.get(i));
+//             curr = curr.right;
+//         }
+
+//         return newRoot;
+//     }
+
+//     public void preorder(TreeNode root, ArrayList<Integer> list){
+
+//         if(root == null) return;
+
+//         list.add(root.val);
+//         preorder(root.left, list);
+//         preorder(root.right, list);
+
+//     }
+// }
+
+
 class Solution {
+
+    TreeNode dummy = new TreeNode(-1);
+    TreeNode curr = dummy;
+
     public TreeNode increasingBST(TreeNode root) {
-        // code here
 
-        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root);
 
-        preorder(root, list);
-
-        Collections.sort(list);
-        int n = list.size();
-
-        TreeNode newRoot = new TreeNode(list.get(0));
-        TreeNode curr = newRoot;
-
-        for(int i=1; i<n; i++){
-            curr.right = new TreeNode(list.get(i));
-            curr = curr.right;
-        }
-
-        return newRoot;
+        return dummy.right;
     }
 
-    public void preorder(TreeNode root, ArrayList<Integer> list){
+    public void inorder(TreeNode root) {
 
-        if(root == null) return;
+        if (root == null) {
+            return;
+        }
 
-        list.add(root.val);
-        preorder(root.left, list);
-        preorder(root.right, list);
+        inorder(root.left);
 
+        root.left = null;
+        curr.right = root;
+        curr = root;
+
+        inorder(root.right);
     }
 }
