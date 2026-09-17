7a8a52496f43a157eeded778aa0b238ecf9dd9c5
Riyanshu Sharma <riyanshu062@gmail.com>
2026-09-17 07:53:40 +0530

    Time: 32 ms (31.64%), Space: 47 MB (29.96%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        // code here
+
+        int n = stones.length;
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        // Create adjacency list
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Create edges
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (stones[i][0] == stones[j][0] ||
+                    stones[i][1] == stones[j][1]) {
+
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
+
+                components++;
+
+                dfs(i, adj, visited);
+            }
+        }
+
+        return n - components;
+    }
+
+    public void dfs(int node, List<List<Integer>> adj, boolean[] visited) {
+
+        visited[node] = true;
+
+        for (int neighbour : adj.get(node)) {
+
+            if (!visited[neighbour]) {
+                dfs(neighbour, adj, visited);
+            }
+        }
+    }
+}
